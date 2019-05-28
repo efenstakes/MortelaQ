90de7d192838462c1de24a710b65cdc327078e10
Efen <efenstakes101@gmail.com>
2019-05-28 19:13:55 +0300

    set up the application routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+// views
+import { AboutComponent } from './layouts/about/about.component'
+
+const routes: Routes = [
+  { path: '', redirectTo: 'user/login', pathMatch: 'full' },
+  { path: 'about', component: AboutComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,34 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { UserModule } from './layouts/user/user.module'
+import { ProductModule } from './layouts/product/product.module'
+import { OrderModule } from './layouts/order/order.module'
+import { InventoryModule } from './layouts/inventory/inventory.module'
+import { ExpenseModule } from './layouts/expense/expense.module'
+import { PenaltyModule } from './layouts/penalty/penalty.module';
+
+import { AboutComponent } from './layouts/about/about.component'
+
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+
+    // internal modules
+    UserModule,
+    ProductModule,
+    OrderModule,
+    InventoryModule, 
+    ExpenseModule,
+    PenaltyModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
